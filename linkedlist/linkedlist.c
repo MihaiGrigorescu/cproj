556d6e6e012606781ccb6582610c905eeb56898b
MihaiGrigorescu <59318189+MihaiGrigorescu@users.noreply.github.com>
2020-06-06 19:28:10 +0300

    comm

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -70,10 +70,5 @@ void main()
 {
     nod* head = initList();
     printf("Valoarea primului nod este %d",head->value);
-<<<<<<< HEAD
-    printf("XXX");
-    
-=======
-    print("trying some new stuff");
->>>>>>> f2d05daee6524be9e1ee2457fae10b5cdc021c36
+    printf("XXX");   
 }
